6d838b355dea5c3e47c73a7e5b68746e11340b61
zcy0108 <62758675+zcy0108@users.noreply.github.com>
2020-03-27 17:35:16 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+import retro
+
+
+def main():
+    env = retro.make("Breakout-Atari2600")
+    observation_size = env.observation_space.shape[0]
+
+    Cases = 10000
+    case = 0
+    time_interval = 0
+    while case < Cases:
+        obs = env.reset()
+        while True:
+            k = env.action_space.sample()
+            print(k)
+            obs, rew, done, info = env.step(k)
+            if rew == 1:
+                print(rew)
+            # obs observation 210*160*3
+            # rew float reward
+            # done boolean
+            # info score 1 & 2
+            env.render()
+            if done:
+                break
+        case += 1
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
